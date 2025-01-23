6b13841953fb9c72d39704ecaa5956bbb8503753
Rishit Pushpkar <rishit.02pushpkar@gmail.com>
2025-01-23 13:08:54 +0530

    file structure fix and delete files

---
+++ src/pages/Experiences.jsx
@@ -0,0 +1,16 @@
+import "../styles/Experiences.css";
+
+export default function Experiences() {
+  return (
+    <div className="experiencesContainer">
+      <header>
+        <figure>
+          {/* <img src={expBanner} alt="" /> */}
+          <h1>Experiences</h1>
+        </figure>
+      </header>
+
+      <section></section>
+    </div>
+  );
+}
